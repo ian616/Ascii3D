387e788b7075970e63f1e32502345a9bcf36af2b
Sungwon Lee <sung1lee@kaist.ac.kr>
2024-10-23 10:54:27 +0000

    feat: model loader

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,21 @@ import useLoader from "../src/engines/loader";
 import "./App.scss";
 
 function App() {
+  const loader = useLoader();
   const { frameBuffer, convertFrameBufferToString } = useASCII3DRenderer(120, 50);
-  useLoader();
+
   const renderWindowRef = useRef<HTMLDivElement>(null);
+
+  useEffect(()=>{
+    loader.parseObjtoPolygons();
+  }, []);
 
   useEffect(() => {
     if (renderWindowRef.current) {
       renderWindowRef.current.innerHTML = convertFrameBufferToString();
     }
   }, [frameBuffer]);
+  
   return (
     <>
       <div className="window" ref={renderWindowRef}></div>
--- src/engines/loader.ts
+++ src/engines/loader.ts
@@ -1,11 +1,40 @@
 import { useEffect } from "react";
-import type { Polygon } from "../entities/polygon";
-import { create, all, Matrix } from 'mathjs';
+import type { Polygon } from "../model/polygon";
+
+import { create, all, Matrix } from "mathjs";
 
 export default function useLoader() {
-    const math = create(all);
+  const math = create(all);
 
-    const parseObjtoPolygons = ()=>{
+  const parseObjtoPolygons = async () => {
+    const ObjString: string = await readModelfromFile("cube");
 
-    };
+    const vertices: Matrix[] = [];
+    const polygons: Polygon[] = [];
+
+    ObjString.split("\n").forEach((row) => {
+      const datas: string[] = row.split(" ");
+
+      if (datas[0] === "v") {
+        vertices.push(math.matrix([datas[1], datas[2], datas[3]]));
+      } else if (datas[0] === "f") {
+        const polygon: Polygon = {
+          vertices: [
+            vertices[parseInt(datas[1]) - 1],
+            vertices[parseInt(datas[2]) - 1],
+            vertices[parseInt(datas[3]) - 1],
+          ],
+        };
+        polygons.push(polygon);
+      }
+    });
+  };
+
+  const readModelfromFile = async (modelName: string) => {
+    const response = await fetch(`/objects/${modelName}.ts`);
+    const fileContent: string = await response.text();
+    return fileContent;
+  };
+
+  return { parseObjtoPolygons };
 }
